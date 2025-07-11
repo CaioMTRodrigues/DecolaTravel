f1b33a9b1111c97ea77912a68d4c4824a3813051
DevPsico <ericsonsergio19844@gmail.com>
2025-07-11 10:28:51 -0300

    Criação da PacoteDto

---
+++ Dtos/PacoteDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DecolaTravel.Dtos
+{
+    /*
+     * Essa classe representa os dados necessários para criar ou atualizar 
+     * um pacote de viagem. Ela é usada para transferir informações entre o 
+     * cliente e a API, garantindo que os dados estejam no formato esperado. 
+     */
+    public class PacoteDto
+    {
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public string Destino { get; set; }
+
+        [Required]
+        public int DuracaoDias { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+
+        [Required]
+        public DateTime DataFim { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        public string? ImagemUrl { get; set; }
+    }
+
+}
