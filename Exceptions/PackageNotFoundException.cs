e06124c3d4eae765a7e69fd9eb24245d472bc882
DevPsico <ericsonsergio19844@gmail.com>
2025-07-11 10:35:46 -0300

    Criação da PackageNotFoundException

---
+++ Exceptions/PackageNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace DecolaTravel.Exceptions
+{
+    /*
+     * Essa classe define uma exceção personalizada para quando um pacote de 
+     * viagem não é encontrado no sistema.
+     */
+    public class PackageNotFoundException : Exception //Herda de Exception: Permite lançar e capturar esse
+                                                     //erro como parte do fluxo de tratamento.
+    {
+        public PackageNotFoundException(int id)
+
+        //Pode personalizar a MSG aqui  ( ERRO específico )
+        : base($"O pacote com ID {id} não foi encontrado. Verifique se o código está correto.") { }
+    }
+
+}
