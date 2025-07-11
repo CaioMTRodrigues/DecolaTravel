d08062e35a66b19237431ca380e3db3caefd69ce
DevPsico <ericsonsergio19844@gmail.com>
2025-07-11 10:31:17 -0300

    Criação da AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DecolaTravel.Data
+{
+    /*
+     *Essa classe representa o contexto de banco de dados da aplicação, usada pelo 
+     *Entity Framework Core para mapear e gerenciar entidades no banco. 
+     */
+
+    public class AppDbContext : DbContext //Classe base do EF Core que gerencia a conexão
+                                          //com o banco e o rastreamento de entidades.
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        /*
+         * DbSet<Pacote> Pacotes: Representa a tabela de pacotes no banco de dados, 
+         * permitindo consultas e operações CRUD.
+         */
+        public DbSet<Pacote> Pacotes { get; set; }
+    }
+}
