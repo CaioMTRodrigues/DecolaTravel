82e2df43de32a1cbeaac66ab508e180a4f38f3b9
DevPsico <ericsonsergio19844@gmail.com>
2025-07-11 10:48:04 -0300

    Criação da Classe Package

---
+++ Models/Package.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DecolaTravel.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string Descricao { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Destino { get; set; }
+
+        [Required]
+        public int DuracaoDias { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+
+        [Required]
+        public DateTime DataFim { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Valor { get; set; }
+
+        public string? ImagemUrl { get; set; }
+    }
+
+}
