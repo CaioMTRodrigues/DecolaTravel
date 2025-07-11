7b5346eb136d248cb8edae53298ab489540959e7
DevPsico <ericsonsergio19844@gmail.com>
2025-07-11 10:42:51 -0300

    Criação da HttpResponseExceptionFilter

---
+++ Filters/HttpResponseExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DecolaTravel.Filters
+{
+    //Esse filtro trata exceções lançadas durante a execução de ações nos controladores da API.
+    public class HttpResponseExceptionFilter : IExceptionFilter //permitindo interceptar exceções
+                                                                //não tratadas.
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is Exceptions.PackageNotFoundException ex)
+            {
+
+                var brasiliaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+                var localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, brasiliaTimeZone);
+
+                context.Result = new NotFoundObjectResult(new
+                {
+                    status = 404,
+                    message = ex.Message,
+                    timestamp = localTime.ToString("dd/MM/yyyy HH:mm:ss") // Formato brasileiro
+                });
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
